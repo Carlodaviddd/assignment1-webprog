000e32172ac14f883cc14d9ebf06aed5987191bd
Carlo David <carlosicatdavid@gmail.com>
2025-07-10 08:46:21 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* highlight the current active page
+https://www.geeksforgeeks.org/how-to-make-active-navbar-in-html-css-and-javascript/ */
+const currentPage = window.location.pathname.split("/").pop();
+const links = document.querySelectorAll(".navbar a");
+
+for (let i = 0; i < links.length; i++) {
+  if (links[i].getAttribute("href") === currentPage) {
+    links[i].classList.add("active");
+    break;
+  }
+}
+
+//after 10seconds, image will display
+setTimeout(function () {
+  document.getElementById("hero").style.backgroundImage =
+    'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("island-moon.jpg")';
+}, 10000);
