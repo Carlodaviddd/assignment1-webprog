a87ee970ac61805f04bf6ca020fbf86b6983c133
Carlo David <carlosicatdavid@gmail.com>
2025-07-14 22:26:48 -0600

    adding mark converter script

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ setTimeout(function () {
   document.getElementById("hero").style.backgroundImage =
     'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("island-moon.jpg")';
 }, 10000);
+
+//mark converter
+const inputGrade = document.getElementById("input-text");
+const convertBtn = document.getElementById("convert-btn");
+const markResult = document.getElementById("result");
+
+convertBtn.addEventListener("click", function () {
+  const inputValue = inputGrade.value.trim();
+  markResult.textContent = inputValue;
+
+  if (inputValue === "") {
+    markResult.textContent = "Grade cannot be empty.";
+  } else if (isNaN(inputValue)) {
+    markResult.textContent = "Please enter a valid number.";
+  } else if (inputValue < 0) {
+    markResult.textContent = "Grade cannot be negative number.";
+  } else if (inputValue > 100) {
+    markResult.textContent = "Grade cannot exceed 100.";
+  } else {
+    let grade = "";
+    if (Number(inputValue) >= 90) {
+      grade = "A";
+    } else if (Number(inputValue) < 90 && Number(inputValue) >= 80) {
+      grade = "B";
+    } else if (Number(inputValue) < 80 && Number(inputValue) >= 70) {
+      grade = "C";
+    } else if (Number(inputValue) < 70 && Number(inputValue) >= 60) {
+      grade = "D";
+    } else if (Number(inputValue) > 60 && Number(inputValue) >= 50) {
+      grade = "E";
+    } else {
+      grade = "F";
+    }
+    markResult.textContent = `Your grade is ${grade}.`;
+  }
+});
